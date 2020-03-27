42f8622242c1e443dc2907b8f5fbf025e1f104ea
Eric Kuang <erickuang@Erics-MacBook-Pro.local>
2020-03-27 18:39:49 +0800

    added a deploy bundle

---
+++ .cache/sync-requires.js
@@ -0,0 +1,10 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/erickuang/Workspace/eric183.github.io/src/pages/index.js")))
+}
+
