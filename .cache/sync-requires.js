982239a816b3c673c70fd7a711a7576086248b04
eric183 <10773980+eric183@users.noreply.github.com>
2020-05-27 10:28:52 +0800

    bugs?  netlify not listened changes?

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,6 +5,7 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("F:\\Workspace\\eric183.github.io\\.cache\\dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("F:\\Workspace\\eric183.github.io\\src\\pages\\404.js"))),
   "component---src-pages-about-js": hot(preferDefault(require("F:\\Workspace\\eric183.github.io\\src\\pages\\about.js"))),
   "component---src-pages-cv-js": hot(preferDefault(require("F:\\Workspace\\eric183.github.io\\src\\pages\\cv.js"))),
