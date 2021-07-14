f4fe459de6425cca1ffe303709a7f34fef0c6e1e
kuangzhichen <kuangzhichen@dingtalkc.com>
2021-07-14 14:48:20 +0800

    updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,36 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "globals": {
-        "__PATH_PREFIX__": true,
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "react-hooks",
-        "@typescript-eslint",
-    ],
-    "rules": {
-    }
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	globals: {
+		__PATH_PREFIX__: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		// 'airbnb',
+		'plugin:@typescript-eslint/recommended',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: [
+			'tsconfig.json',
+		],
+		tsconfigRootDir: __dirname,
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	plugins: [
+		'react',
+		'react-hooks',
+		'@typescript-eslint',
+	],
+	rules: {
+		'no-use-before-define': 'off',
+	},
 };
