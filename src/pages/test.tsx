398ba7beb0a621a5acdf6d693f2b325cbdab853d
kuangkuang <kk297466058@aliyun.com>
2021-07-18 18:37:31 +0800

    updated

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,42 +1,54 @@
 import { Box, Icosahedron, Octahedron, OrbitControls, ScreenQuad, Sphere, TransformControls, useHelper } from '@react-three/drei';
 import { useFrame, useLoader, useThree } from '@react-three/fiber';
+import { Bloom, EffectComposer } from '@react-three/postprocessing';
 import { button, useControls } from 'leva';
 import React, { FC, Suspense, useEffect, useRef, useState } from 'react';
 import { MathUtils, BoxHelper, PointLightHelper, SpotLightHelper, TextureLoader, Vector3 } from 'three';
+import { KernelSize } from 'postprocessing';
 
 import { TransformControls as TransformControlsImpl } from 'three-stdlib'
 import CanvasLayout from '~components/CanvasLayout';
 import TransformTool from '~components/TransformTool';
 
 
-const Test: FC = ()=> {
+const Test: FC = () => {
     const [isControl, setIsControl] = useState(true);
     // const size = useThree((state) => state.size)
     const ScreenQuadRef = useRef(null!)
-    
+
     useEffect(() => {
         // console.log(ScreenQuadRef);
         // console.log(MathUtils.lerp(1, 10, 0.1));
     }, []);
-    
+
     return (
-        <CanvasLayout wrapperStyle={{ background: 'rgb(24, 24, 24)'}}>
-                <Suspense fallback={null}>
-            {/* <ScreenQuad 
+        <CanvasLayout wrapperStyle={{ background: 'rgb(24, 24, 24)' }}>
+            <Suspense fallback={null}>
+                {/* <ScreenQuad 
                 ref={ScreenQuadRef}
             >    */}
-                    <MeshGroup />
-                    <LightGroup controlArgs={[isControl, setIsControl]} />
-                    {/* <OrbitControls enabled={isControl}/> */}
-            {/* </ScreenQuad> */}
-                </Suspense>
+                <MeshGroup />
+                <LightGroup controlArgs={[isControl, setIsControl]} />
+                {/* <OrbitControls enabled={isControl}/> */}
+                {/* </ScreenQuad> */}
+            </Suspense>
+            <EffectComposer multisampling={8}>
+                {/* <Bloom /> */}
+                <Bloom
+                    kernelSize={3}
+                    luminanceThreshold={0}
+                    luminanceSmoothing={0.4}
+                    intensity={0.6}
+                />
+                <Bloom kernelSize={KernelSize.HUGE} luminanceThreshold={0} luminanceSmoothing={0} intensity={0.5} />
+            </EffectComposer>
         </CanvasLayout>
     )
-} 
+}
 
 const MeshGroup = () => {
     const [vec] = useState(() => new Vector3())
-    const [normalTexture] = useLoader(TextureLoader,  ['/NormalMap.png']);
+    const [normalTexture] = useLoader(TextureLoader, ['/NormalMap.png']);
     const mesh = useRef<THREE.Mesh>(null!);
     const sphereRef = useRef<THREE.Mesh>(null!);
     const { mouse } = useThree();
@@ -54,8 +66,11 @@ const MeshGroup = () => {
         // console.log('rotation:', mesh.current.rotation);
         // console.log('position:', mesh.current.position.x);
         // if (start) {
-            state.camera.position.lerp(vec.set(state.mouse.x * 10,  state.mouse.y * 10, 14), 0.05)
-            state.camera.lookAt(0, 0, 0)
+        state.camera.position.lerp(
+            vec.set(state.mouse.x * 10, state.mouse.y * 10, 6),
+            0.05
+        );
+        state.camera.lookAt(0, 0, 0);
         //   }
     });
 
@@ -73,14 +88,14 @@ const MeshGroup = () => {
             <mesh>
                 {/* <Sphere ref={sphereRef}> */}
                 {/* <Sphere attach='sphere'> */}
-                    {/* <meshNormalMaterial /> */}
-                    <sphereBufferGeometry args={[1.1, 30, 30]} attach="geometry" />
-                    <meshStandardMaterial 
-                        color={'0x292929'}
-                        normalMap={normalTexture}
-                        metalness={0.1}
-                        roughness={.2}
-                    />
+                {/* <meshNormalMaterial /> */}
+                <sphereBufferGeometry args={[1.1, 30, 30]} attach="geometry" />
+                <meshStandardMaterial
+                    color={'0x292929'}
+                    normalMap={normalTexture}
+                    metalness={0.1}
+                    roughness={.2}
+                />
                 {/* </Sphere> */}
             </mesh>
         </group>
@@ -93,26 +108,34 @@ const LightGroup: FC<{
 }> = ({
     controlArgs
 }) => {
-    const LightGroupRef = useRef();
-    const transformControlsRef = useRef<TransformControlsImpl>(null!);
-    const pl1 = useRef();
-    
-    useHelper(pl1, PointLightHelper, 0.5, 'cyan')
+        const LightGroupRef = useRef();
+        const transformControlsRef = useRef<TransformControlsImpl>(null!);
+        const pl1 = useRef();
 
-    const { position } = useControls({
-        position: [1, 1, 1]
-    });
-  
-    return (
-        <group ref={LightGroupRef}>
-            {/* <ambientLight args={[0x4, 2]}/> */}
-            {/* <TransformControls position={position} ref={transformControlsRef}> */}
-            <TransformTool controlArgs={controlArgs} tKey="pl1">
+        useHelper(pl1, PointLightHelper, 0.5, 'cyan')
+
+        const { position } = useControls({
+            position: [1, 1, 1]
+        });
+
+        return (
+            <group ref={LightGroupRef}>
+                <ambientLight args={[0x4, 2]} />
+                {/* <TransformControls position={position} ref={transformControlsRef}> */}
+                {/* <TransformTool controlArgs={controlArgs} tKey="pl1">
                 <pointLight args={[0xffffff, 1, 2]} ref={pl1} />
-            </TransformTool>
-            {/* </TransformControls> */}
-        </group>
-    )
-}
+            </TransformTool> */}
+                {/* </TransformControls> */}
+            </group>
+        )
+    }
+
+// const Effects = () => {
+//     return (
+//         <group>
+
+//         </group>
+//     )
+// }
 
 export default Test;
